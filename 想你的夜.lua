3925a3ae2d70fd8b64bcb0acf429503145385876
Guo61 <gg1457o@qq.com>
2025-08-31 00:45:49 +0800

    Add files via upload

--- 想你的夜.lua
+++ 想你的夜.lua
@@ -105,6 +105,23 @@ Tabs.Home:Button({
     end
 })
 
+Tabs.Home:Button("空降极速传奇",function()
+local game_id = 3101667897
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+Tabs.Home:Button("空降忍者传奇",function()
+local game_id = 3956818381
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
+
+Tabs.Home:Button("空降力量传奇",function()
+local game_id = 3623096087
+        local game_url = "https://www.roblox.com/games/"..game_id
+        game:GetService("TeleportService"):Teleport(game_id, game.Players.LocalPlayer)
+end)
 -- FPS 显示
 Tabs.Home:Toggle({
     Title = "显示FPS",
